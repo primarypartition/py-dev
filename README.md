05dc08d424981b32bf32a0532662015dbcf34226
vagrant <vagrant@master>
2020-05-19 19:33:49 +0000

    html, css

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Python Project
 
-## Project Codepen
+## Projects
 
-### HTML, CSS
+### HTML, CSS, Bootstrap
 
 > https://codepen.io/primarypartition/project/editor/ZOgQbR
+> https://codepen.io/primarypartition/pen/jObXbEO
+
+### JavaScript
+
+> https://codepen.io/primarypartition/pen/rNOoLZG
 
 
-## Bootstrap
+## Configure Bootstrap
 
 > https://getbootstrap.com/docs/3.3/
 
---
+++ js/basic-js.js
@@ -0,0 +1,26 @@
+//alert("Hello World.")
+
+var bankAccount = 100;
+var deposit = 50;
+var total = account + deposit;
+
+var greeting = "Welcome back: ";
+var name = "Jose";
+
+alert(greeting+name)
+
+// undefined (created but not defined)
+var myvariable
+
+// null ("nothing" that you set)
+var bonus = null
+
+// Alert Pop-up
+alert("hello world")
+
+// Output to console
+console.log("Hello world")
+
+// Accept Prompt Inputs
+var age = prompt("How old are you?")
+
